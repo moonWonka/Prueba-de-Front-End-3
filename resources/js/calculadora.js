7b5ecdb152991457655e1986cd5cb0a5171809f8
willy <w.lunah@protonmail.com>
2022-11-20 01:46:07 -0300

    eliminar lista

--- resources/js/calculadora.js
+++ resources/js/calculadora.js
@@ -1,54 +1,55 @@
 const calcular = () => {
-  const operation = document.getElementById("operation").value;
-  const resultado = [];
-  let resultadoA;
-  const rex = /\d+[\-\/\+\*]\d+/;
-  const rexOp = /[\-\/\+\*]/;
+    const operation = document.getElementById('operation').value
+    const resultado = []
+    let resultadoA
 
-  let found = operation.match(rex);
+    const rex = /\d+[\-\/\+\*]\d+/
+    const rexOp = /[\-\/\+\*]/
 
-  if (found === null) {
-    alert("error debe ingresar una operacion correcta");
-  } else {
-    const operador = found[0].match(rexOp);
-    const ope = operador[0];
-    const numeros = operation.split(ope);
+    let found = operation.match(rex)
 
-    numeros.forEach((num) => {
-      const value = parseInt(num);
-      resultado.push(value);
-    });
+    if (found === null) {
+        alert('error debe ingresar una operacion correcta')
+    } else {
+        const operador = found[0].match(rexOp)
+        const ope = operador[0]
+        const numeros = operation.split(ope)
 
-    switch (ope) {
-      case "+":
-        resultadoA = resultado[0] + resultado[1];
-        document.getElementById(
-          "result"
-        ).innerHTML = `el resultado de sumar ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`;
-        break;
-      case "-":
-        resultadoA = resultado[0] - resultado[1];
-        document.getElementById(
-          "result"
-        ).innerHTML = `el resultado de restar ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`;
-        break;
-      case "*":
-        resultadoA = resultado[0] * resultado[1];
-        document.getElementById(
-          "result"
-        ).innerHTML = `el resultado de multiplicar ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`;
-        break;
-      case "/":
-        resultadoA = resultado[0] / resultado[1];
-        document.getElementById(
-          "result"
-        ).innerHTML = `el resultado de dividir ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`;
-        break;
-      default:
-        console.log("error de operador!");
-        break;
+        numeros.forEach((num) => {
+            const value = parseInt(num)
+            resultado.push(value)
+        })
+
+        switch (ope) {
+            case '+':
+                resultadoA = resultado[0] + resultado[1]
+                document.getElementById(
+                    'result'
+                ).innerHTML = `el resultado de sumar ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`
+                break
+            case '-':
+                resultadoA = resultado[0] - resultado[1]
+                document.getElementById(
+                    'result'
+                ).innerHTML = `el resultado de restar ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`
+                break
+            case '*':
+                resultadoA = resultado[0] * resultado[1]
+                document.getElementById(
+                    'result'
+                ).innerHTML = `el resultado de multiplicar ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`
+                break
+            case '/':
+                resultadoA = resultado[0] / resultado[1]
+                document.getElementById(
+                    'result'
+                ).innerHTML = `el resultado de dividir ${resultado[0]} y ${resultado[1]} es: ${resultadoA}`
+                break
+            default:
+                console.log('error de operador!')
+                break
+        }
+
+        console.log(resultadoA)
     }
-
-    console.log(resultadoA);
-  }
-};
+}
--- resources/js/fibonacci.js
+++ resources/js/fibonacci.js
@@ -1,61 +1,73 @@
 const calcularFibo = (num) => {
-  let n = 0;
-  const serieFibo = [];
+    let n = 0
+    const serieFibo = []
 
-  for (n; n < num; n++) {
-    if (n === 0) {
-      serieFibo.push(0);
-    } else if (n === 1) {
-      serieFibo.push(1);
-    } else {
-      let operacion = serieFibo[n - 1] + serieFibo[n - 2];
-      serieFibo.push(operacion);
+    for (n; n < num; n++) {
+        if (n === 0) {
+            serieFibo.push(0)
+        } else if (n === 1) {
+            serieFibo.push(1)
+        } else {
+            let operacion = serieFibo[n - 1] + serieFibo[n - 2]
+            serieFibo.push(operacion)
+        }
     }
-  }
 
-  return serieFibo;
-  //0 1 1 2 3 5 8
-};
+    return serieFibo
+    //0 1 1 2 3 5 8
+}
 
 const comprobar = (str) => {
-  const value = parseInt(str);
-  const revisar = isNaN(value);
+    const value = parseInt(str)
+    const revisar = isNaN(value)
 
-  if (revisar || value < 1) {
-    alert("debe ingresar un numero positivo");
-    return false;
-  }
+    if (revisar || value < 1) {
+        alert('debe ingresar un numero positivo')
+        return false
+    }
 
-  return true;
-};
+    return true
+}
 
 const agregarSerie = (secuencia) => {
-  console.log("que pasa");
-  const nodo = document.getElementById("secuencia");
-  console.log("olita");
+    const nodo = document.getElementById('secuencia')
 
-  secuencia.forEach((ele) => {
-    const li = document.createElement("li");
-    const txtNode = document.createTextNode(ele);
+    const crearUl = document.createElement('ul')
 
-    li.appendChild(txtNode);
-    nodo.appendChild(li);
-  });
-};
+    secuencia.forEach((ele) => {
+        const li = document.createElement('li')
+        const txtNode = document.createTextNode(ele)
+
+        li.appendChild(txtNode)
+        nodo.appendChild(li)
+    })
+}
 
 const mostrarFibo = () => {
-  let secuencia;
-  const target = document.getElementById("txt_number").value;
-  //console.log(target)
-  const comprobacion = comprobar(target);
+    let secuencia
+    //comprobar si es un numero
+    const target = document.getElementById('txt_number').value
+    const comprobacion = comprobar(target)
+    //comprobar si hay elementos <li>
+    const largoLi = document.querySelectorAll('li').length > 0
 
-  if (comprobacion) {
-    const cantidad = parseInt(target);
-    secuencia = calcularFibo(cantidad);
-    agregarSerie(secuencia);
-    //document.getElementById('txt_number').value = ""
-    console.log(secuencia);
-  } else {
-    document.getElementById("txt_number").value = "";
-  }
-};
+    if (largoLi) {
+        eliminarSerie()
+    }
+
+    if (comprobacion) {
+        //transformar a int el numero
+        const cantidad = parseInt(target)
+        //crear secuencia
+        secuencia = calcularFibo(cantidad)
+        //agregar secuencia a los li
+        agregarSerie(secuencia)
+    } else {
+        document.getElementById('txt_number').value = ''
+    }
+}
+
+const eliminarSerie = () => {
+    const target = document.querySelector('.uwu')
+    target.innerHTML = ''
+}
